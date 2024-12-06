734b8cd033775e0baa55749423ae23f35b3f153f
Andrew Passanisi <andrew@specbooks.com>
2024-12-06 11:53:46 -0800

    first commit

--- plugins/snipcart.js
+++
@@ -1,9 +0,0 @@
-import { defineNuxtPlugin } from '#app'
-import { createApp } from 'vue'
-import { Snipcart } from 'snipcart'
-
-export default defineNuxtPlugin((nuxtApp) => {
-  const app = createApp()
-  app.component('Snipcart', Snipcart)
-  nuxtApp.vueApp.use(app)
-})
