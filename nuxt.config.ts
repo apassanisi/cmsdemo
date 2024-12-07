0da2fbb8eaf8ef94c448e9cbfb39e385189d687b
Andrew Passanisi <andrew@specbooks.com>
2024-12-07 08:41:40 -0800

    second commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
     head: {
       script: [
         { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' },
+        { src: 'https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js' },
       ],
     },
   },
